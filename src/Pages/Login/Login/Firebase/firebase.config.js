b86ba22d73f9f9457b0bf84b5783a838be534d47
Naim365 <shahjadanaimuddin@gmail.com>
2021-12-30 10:56:58 +0600

    stripe client side

---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,22 @@
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+// };
+// export default firebaseConfig;
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCcah9EOa-MSM8MDemWUK8Uhlts6Ysi_T4",
+    authDomain: "doctors-appointments-booking.firebaseapp.com",
+    projectId: "doctors-appointments-booking",
+    storageBucket: "doctors-appointments-booking.appspot.com",
+    messagingSenderId: "54497696486",
+    appId: "1:54497696486:web:f8966238ae572add9e7927"
+  };
+
+
+  export default firebaseConfig;
